2ac62089f0c3f79e62c4992585c698b1d8895466
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-01-12 23:39:33 +0530

    Update noDouble.java

--- double/noDouble.java
+++ double/noDouble.java
@@ -1,7 +1,15 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int l=nums.length;
-        for(int i=0;i<)
+        int j=1;
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i]!=nums[i+1])
+            {
+                nums[j]=nums[i+1];
+                j++;
+            }
         
+    }
+    return j;
     }
 }
