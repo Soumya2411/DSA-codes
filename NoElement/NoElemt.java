22e73de35f56bf7c5028125a8756277abf6b4fc3
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-01-13 21:27:16 +0530

    Create NoElemt.java

---
+++ NoElement/NoElemt.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int l=nums.length;
+         ArrayList<Type> list = new ArrayList<>();
+
+        for(int i=0;i<l;i++)
+        {
+            
+            if(nums[i]!=val)
+        arr[i]=nums[i];
+        
+        }
+        return arr;
+
+    }
+
+}
