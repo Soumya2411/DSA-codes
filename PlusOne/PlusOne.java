6ab744c4da43fed9c2adc8c67081964454236cd4
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-01-15 22:30:34 +0530

    Update PlusOne.java

--- PlusOne/PlusOne.java
+++ PlusOne/PlusOne.java
@@ -2,5 +2,19 @@ package PlusOne;
 
 public class PlusOne {
     
-    
+    class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+	if (digits[i] < 9) {
+		digits[i]++;
+		return digits;
+	}
+	digits[i] = 0;
+}
+
+digits = new int[digits.length + 1];
+digits[0] = 1;
+return digits;
+}
+}
 }
