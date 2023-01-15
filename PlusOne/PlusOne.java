a86117d8d5f477b47984346d817f594991ed9241
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-01-15 22:38:38 +0530

    Update PlusOne.java

--- PlusOne/PlusOne.java
+++ PlusOne/PlusOne.java
@@ -5,15 +5,17 @@ public class PlusOne {
     class Solution {
     public int[] plusOne(int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
-	if (digits[i] < 9) {
-		digits[i]++;
-		return digits;
-	}
-	digits[i] = 0;
+            //ulta loop
+	if (digits[i] < 9) { //[4]
+        //[1,4,4]
+		digits[i]++; //[5]
+		return digits; //[1,4,5]
+	} //else
+	digits[i] = 0; //9 se niche ni to 9 hoga aur usme +1 hua to 0 ho jyga//
 }
 
-digits = new int[digits.length + 1];
-digits[0] = 1;
+digits = new int[digits.length + 1]; //pr agr aisa hua for example = [9,9,9] +1 = 1000 to phle to ek space brhao 
+digits[0] = 1; // aur phle place p zero
 return digits;
 }
 }
