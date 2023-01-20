66ce4b87dcd771bba22773a58eded7b86fbe9d35
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-01-20 21:43:49 +0530

    Update MoveZeroes.java

--- MoveZeroes/MoveZeroes.java
+++ MoveZeroes/MoveZeroes.java
@@ -1,9 +1,17 @@
 
 class moveZero{
     public static void main(int[] nums){
+        int snowball=0,t;
 for(int i=0;i<nums.length;i++)
 {
-
+if(nums[i] = 0 ){
+    snowball++;
+}
+else if{
+    t=nums[i];
+    nums[i]=0;
+    nums[i-snowball] = t;
+}
 }
     }
 }
