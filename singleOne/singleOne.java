d4a3ad41092c9ad8795f03e320824c796856bd10
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-01-22 01:42:41 +0530

    Update singleOne.java

--- singleOne/singleOne.java
+++ singleOne/singleOne.java
@@ -1,5 +1,16 @@
 package singleOne;
 
 public class singleOne {
-    
+    class Solution {
+        public int singleNumber(int[] nums) {
+            Arrays.sort(nums);
+            for(int i=0;i<nums.length-1;i++){
+                if(nums[i]!=nums[i+1]){
+                    return nums[i];
+                }
+                    i++; //shifting becuase do digit same h
+            }
+            return nums[nums.length-1];
+        }
+    }
 }
