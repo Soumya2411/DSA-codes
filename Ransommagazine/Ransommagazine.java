a1a15e6a4fd84067286c7fcf64d513579f45de33
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-01-25 23:33:28 +0530

    Create Ransommagazine.java

---
+++ Ransommagazine/Ransommagazine.java
@@ -0,0 +1,25 @@
+package Ransommagazine;
+
+public class Ransommagazine {
+    String ransom, String magazine;
+    int [] alpha=new int[26];
+    if(ransom.length()>magazine.length()){
+        return false;
+
+    }
+    else{
+        for(char c:magazine.toCharArray()){
+alpha[c-'a']++;
+
+for(char c:ransom.toCharArray()){
+    if(apha[c-'a'==0]){
+        return false;
+    }
+    else{
+        alpha[c-'a']--;
+    }
+    return true;
+}
+        }
+    }
+}
