3e4cb0831f4cfcea3d4d079c8fcee1b74e0d1bbb
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-01-29 22:24:28 +0530

    updateagain

--- MaxSumSubarray/MaxSumSubarray.java
+++ MaxSumSubarray/MaxSumSubarray.java
@@ -3,7 +3,7 @@ class Solution {
         //[5,4,-1,7,8]
         //
         int cursum=0;
-        int maxsum=0;
+        int maxsum=nums[0];
         for(int i=0;i<nums.length;i++){
             cursum+=nums[i];
             if(cursum>maxsum){
---
+++ twosum/twosum.java
@@ -0,0 +1,5 @@
+package twosum;
+
+public class twosum {
+    
+}
