c88d9a968150f09a77680f3a9cdb51fe9e03c718
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-01-29 23:06:01 +0530

    updatee

--- twosum/twosum.java
+++ twosum/twosum.java
@@ -1,5 +1,16 @@
 package twosum;
 
 public class twosum {
-    
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans=new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    ans[0]=i;
+                    ans[1]=j;
+                }
+            }
+        }
+        return ans;
+    }
 }
