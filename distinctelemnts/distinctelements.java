f35e285bcf3c8568ef7a47acdfc684e3eb5c2d60
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-01-31 22:39:15 +0530

    Create distinctelements.java

---
+++ distinctelemnts/distinctelements.java
@@ -0,0 +1,18 @@
+package distinctelemnts;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class distinctelements {
+    //in a set hashset every element can be stored only once//
+    int count distinct[int[] a]{
+        Set<E> <Integer> set=new HashSet<>();
+        for(int element: a){
+            set.add(element); 
+            //ina a set every element can be added only once so to check distinct elemnts we are adding it in the set and then taking back the size//
+        }
+        return set.size();
+    }
+
+
+}
