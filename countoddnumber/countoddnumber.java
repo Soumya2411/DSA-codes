44c96967937fc6af2cd6f1d7c7e8896fdf7fc352
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-02-13 16:16:03 +0530

    Update countoddnumber.java

--- countoddnumber/countoddnumber.java
+++ countoddnumber/countoddnumber.java
@@ -1,5 +1,15 @@
 package countoddnumber;
 
 public class countoddnumber {
-    
+    int range=high-low;
+    if(low%2==1&&high%2==1){
+        return (range/2)+1;
+    }
+    if(low%2==0&&high%2==0){
+        return range/2;
+        
+    }
+    else{
+        else return (int)Math.ceil((double)range/2);
+    }
 }
