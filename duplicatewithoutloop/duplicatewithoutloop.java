d3ab95d7189eecc37d60b4b8ccd953c127e25969
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-02-16 18:11:42 +0530

    Create duplicatewithoutloop.java

---
+++ duplicatewithoutloop/duplicatewithoutloop.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return num;
+            } else {
+                set.add(num);
+            }
+        }
+        return -1; // or any other value to indicate no duplicate found
+    }
+}
+
+    
+
