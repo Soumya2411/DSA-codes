b0dde4fcb058b36ded424f32262add7036f15f66
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-02-18 22:04:20 +0530

    Create reversebinarytree.java

---
+++ rversebinarytree/reversebinarytree.java
@@ -0,0 +1,17 @@
+
+    class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            if(root==null){
+                return null;
+    
+            }
+    
+            TreeNode temp=root.left;
+            root.left=root.right;
+            root.right=temp;
+            invertTree(root.left);
+            invertTree(root.right);
+            return root;
+        }
+    }
+
