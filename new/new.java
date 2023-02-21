45b6c6fbcc4f86a98a70601da2369c04565df452
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-02-21 23:54:20 +0530

    Update new.java

--- new/new.java
+++ new/new.java
@@ -1,5 +1,28 @@
-import java.util.*;package new;
-
-public class new {
-    
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        int l = 0;
+        int r = n-1;
+        
+        if(n==1)
+            return nums[0];
+        if(nums[0] != nums[1])
+            return nums[0];
+        if(nums[n-1] != nums[n-2])
+            return nums[n-1];
+        
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            
+            if(mid>0 && mid < n-1){
+                if(nums[mid] > nums[mid-1] && nums[mid] < nums[mid+1])
+                    return nums[mid];
+                else if((nums[mid] == nums[mid-1] && mid%2==1) || (nums[mid] == nums[mid+1] && mid%2==0))
+                    l = mid+1;
+                else 
+                    r = mid -1;
+            }
+        }
+        return 0;
+    }
 }
